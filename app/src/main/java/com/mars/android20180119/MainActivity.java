f54dba9f3ab6ebc018ec3c540034214e48a30c15
MarsChang <mars09250148@yahoo.com.tw>
2018-01-19 11:09:53 +0800

    Initial commit

---
+++ app/src/main/java/com/mars/android20180119/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mars.android20180119;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//講解fragment layout
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
